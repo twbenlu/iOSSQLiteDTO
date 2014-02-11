96ec43ded7489ed5fe518a192c5d139c9340e9b8
benlu <benlu@benlutekiMacBook-Pro.local>
2014-02-11 20:25:49 +0800

    Initial Commit

---
+++ Sqlite_list/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Sqlite_list
+//
+//  Created by benlu on 2/11/14.
+//  Copyright (c) 2014 benlu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
